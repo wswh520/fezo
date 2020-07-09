b70c2b0197b2ebe615e8cabdb9820f008e43efc3
wanghui <1446218560@qq.com>
2020-07-09 16:54:18 +0800

    王慧提交

--- src/main/java/com/fz/test.java
+++ src/main/java/com/fz/test.java
@@ -1,5 +1,6 @@
 package com.fz;
 
+import com.fz.common.util.MD5;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 
 import java.text.DecimalFormat;
@@ -21,72 +22,90 @@ public class test {
 
 
 
-        int no = 721;
-        String str = "";
-        //学校
-        int school = Math.floorMod(no,3);
-        System.out.println(school);
-        String schoolK = no % 12 + "";
-        System.out.println(schoolK);
+//        int no = 721;
+//        String str = "";
+//        //学校
+//        int school = Math.floorMod(no,3);
+//        System.out.println(school);
+//        String schoolK = no % 12 + "";
+//        System.out.println(schoolK);
+//
+//        //第一场
+//        List st1 = Arrays.asList(new String[]{"1","2","3"});
+//        //第二场
+//        List st2 = Arrays.asList(new String[]{"4","5","6"});
+//        //第三场
+//        List st3 = Arrays.asList(new String[]{"7","8","9"});
+//        //第四场
+//        List st4 = Arrays.asList(new String[]{"10","11","0"});
+//
+//        if(school == 1){
+//            if(st1.contains(schoolK)){
+//                str += "J01";
+//            }else if(st2.contains(schoolK)){
+//                str += "J02";
+//            }else if(st3.contains(schoolK)){
+//                str += "J03";
+//            }else{
+//                str += "J04";
+//            }
+//        }else if(school == 2){
+//            if(st1.contains(schoolK)){
+//                str += "Z01";
+//            }else if(st2.contains(schoolK)){
+//                str += "Z02";
+//            }else if(st3.contains(schoolK)){
+//                str += "Z03";
+//            }else{
+//                str += "Z04";
+//            }
+//        }else{
+//            if(st1.contains(schoolK)){
+//                str += "S01";
+//            }else if(st2.contains(schoolK)){
+//                str += "S02";
+//            }else if(st3.contains(schoolK)){
+//                str += "S03";
+//            }else{
+//                str += "S04";
+//            }
+//        }
+//        System.out.println(str);
+//
+//        //座位号
+//        DecimalFormat dft = new DecimalFormat("000");
+//        String seatNo = String.valueOf(dft.format(Math.ceil((float)no/12)));
+//        System.out.println(str + seatNo);
+//
+//        if(no > 720){
+//            if(school == 1){
+//                str = "J04";
+//            }else if(school == 2){
+//                str = "Z04";
+//            }else{
+//                str = "S04";
+//            }
+//            System.out.println(str + seatNo);
+//        }
 
-        //第一场
-        List st1 = Arrays.asList(new String[]{"1","2","3"});
-        //第二场
-        List st2 = Arrays.asList(new String[]{"4","5","6"});
-        //第三场
-        List st3 = Arrays.asList(new String[]{"7","8","9"});
-        //第四场
-        List st4 = Arrays.asList(new String[]{"10","11","0"});
+//        String password = "123456";
+//        System.out.println(MD5.getMD5String(password));
 
-        if(school == 1){
-            if(st1.contains(schoolK)){
-                str += "J01";
-            }else if(st2.contains(schoolK)){
-                str += "J02";
-            }else if(st3.contains(schoolK)){
-                str += "J03";
-            }else{
-                str += "J04";
-            }
-        }else if(school == 2){
-            if(st1.contains(schoolK)){
-                str += "Z01";
-            }else if(st2.contains(schoolK)){
-                str += "Z02";
-            }else if(st3.contains(schoolK)){
-                str += "Z03";
-            }else{
-                str += "Z04";
-            }
-        }else{
-            if(st1.contains(schoolK)){
-                str += "S01";
-            }else if(st2.contains(schoolK)){
-                str += "S02";
-            }else if(st3.contains(schoolK)){
-                str += "S03";
-            }else{
-                str += "S04";
-            }
-        }
-        System.out.println(str);
+        // TODO Auto-generated method stub
+        new ThreadFezo().start();
 
-        //座位号
-        DecimalFormat dft = new DecimalFormat("000");
-        String seatNo = String.valueOf(dft.format(Math.ceil((float)no/12)));
-        System.out.println(str + seatNo);
+        new ThreadFezo().start();
+        new ThreadFezo().start();
+        new ThreadFezo().start();
+        new ThreadFezo().start();
+        new ThreadFezo().start();
+        new ThreadFezo().start();
+        new ThreadFezo().start();
 
-        if(no > 720){
-            if(school == 1){
-                str = "J04";
-            }else if(school == 2){
-                str = "Z04";
-            }else{
-                str = "S04";
-            }
-            System.out.println(str + seatNo);
-        }
 
+    }
+
+    public void test(){
 
     }
 }
